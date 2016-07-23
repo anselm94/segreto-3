4193a24e2d6edb8c7343c0a7b386435e2019d8e3
anselm94 <merbinjanselm@gmail.com>
2016-07-23 15:23:32 +0530

    Updated setup info

--- setup.py
+++ setup.py
@@ -1,7 +1,6 @@
 from setuptools import setup, find_packages
 
-setup(
-      name='segreto3',
+setup(name='segreto3',
       packages=find_packages(),
       version='0.1',
       description='A simple secret ideating app with AES256 encryption, written with Kivy and Flat UI themed',
@@ -11,17 +10,16 @@ setup(
       author='Merbin J Anselm',
       author_email='merbinjanselm@gmail.com',
       license=open('LICENSE.txt').read(),
-      requires = ["simple-crypt", "jsonpickle", "kivy"],
-      keywords = ["kivy", "note", "app", "application", "flat design", "themed", "secret"],
-      classifiers = [
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3",
-        "Development Status :: 4 - Beta",
-        "Environment :: Other Environment",
-        "Intended Audience :: End Users/Desktop",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-        "Topic :: Utilities"    ,
-        "Topic :: Multimedia :: Graphics :: Graphics Conversion",
-        ],
-      )
+      install_requires=["simple-crypt", "jsonpickle", "kivy"],
+      keywords=["kivy", "note", "app", "application",
+                "flat design", "themed", "secret"],
+      classifiers=[
+          "Programming Language :: Python",
+          "Programming Language :: Python :: 3",
+          "Development Status :: 4 - Beta",
+          "Environment :: Other Environment",
+          "Intended Audience :: End Users/Desktop",
+          "License :: OSI Approved :: MIT License",
+          "Operating System :: OS Independent",
+          "Topic :: Utilities"
+      ])
